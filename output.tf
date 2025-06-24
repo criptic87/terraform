f4f1c51cc82835df5b06f336aafc5dde58840136
criptic87 <andrei.ciorogar@gmail.com>
2025-06-24 11:46:21 +0300

    final output file

--- output.tf
+++ output.tf
@@ -6,10 +6,3 @@ output "key_pair_name" {
   value = aws_key_pair.generated.key_name
 }
 
-output "private_ip" {
-  value = aws_instance.ubuntu_public.private_ip
-}
-
-output "key_pair_name" {
-  value = aws_key_pair.generated.key_name
-}
