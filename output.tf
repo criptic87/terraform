a9ecd392c7bb0252f03d25f910368f14ed3acec2
criptic87 <andrei.ciorogar@gmail.com>
2025-06-24 11:33:46 +0300

    outputs commit

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "private_ip" {
+  value = aws_instance.ubuntu_public.private_ip
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.generated.key_name
+}
+
+output "private_ip" {
+  value = aws_instance.ubuntu_public.private_ip
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.generated.key_name
+}
